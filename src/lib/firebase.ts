0375c212a2a702a5be230e45daebb5b7951e5e1c
appsatuan <arif.stwan@outlook.com>
2026-03-31 07:07:06 +0700

    Fix Firebase PEM error and add getApps check

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-import { initializeApp, cert } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-export const firebaseApp = initializeApp({
+export const firebaseApp = getApps().length === 0 ? initializeApp({
     credential: cert({
         clientEmail: process.env.client_email,
-        privateKey: process.env.private_key,
+        // Mengubah teks \n menjadi karakter baris baru (newline) yang sebenarnya
+        privateKey: process.env.private_key ? process.env.private_key.replace(/\\n/g, '\n') : undefined,
+        
         projectId: process.env.project_id,
     })
-});
+}) : getApps()[0];
 
 export const firestoreApp = getFirestore(firebaseApp);
-
-
-
